9c3b8af826f3a2c66588053cdaee7987a5c781b6
Graham Savage <savage@gragsie.com>
2013-05-11 09:31:12 -0400

    eula

---
+++ Abacus/EULAViewController.h
@@ -0,0 +1,13 @@
+//
+//  EULAViewController.h
+//  Abacus
+//
+//  Created by Graham Savage on 5/11/13.
+//  Copyright (c) 2013 Graham Savage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EULAViewController : UIViewController
+
+@end
---
+++ Abacus/EULAViewController.m
@@ -0,0 +1,26 @@
+//
+//  EULAViewController.m
+//  Abacus
+//
+//  Created by Graham Savage on 5/11/13.
+//  Copyright (c) 2013 Graham Savage. All rights reserved.
+//
+
+#import "EULAViewController.h"
+
+@interface EULAViewController ()
+
+@end
+
+@implementation EULAViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)dealloc {
+    
+    [super dealloc];
+}
+@end
